46677fb6fbdacda5478b36186d8be0ea52bf3d50
Covaga <covaga94@gmail.com>
2024-07-30 16:14:14 +0200

    Update event_logger.cs

--- event_logger.cs
+++ event_logger.cs
@@ -2,25 +2,45 @@ using System;
 using System.IO;
 using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.Scripting;
-public class Eventlogger
+using Eplan.EplApi.Base;
+
+public class EventLogger
 {
+    private const string LOG_FILE_PATH = @"C:\Test\Events.txt";
+
     [DeclareEventHandler("onActionEnd.String.*")]
-    public long MyEventHandlerFunction2(IEventParameter iEventParameter)
+    public long MyEventHandlerFunction(IEventParameter iEventParameter)
     {
         try
         {
             EventParameterString oEventParameterString = new EventParameterString(iEventParameter);
-            String strActionName = oEventParameterString.String;
-            StreamWriter sw;
-            FileInfo fi = new FileInfo(@"C:\Test\Events.txt");
-            sw = fi.AppendText();
-            sw.WriteLine("oCLI.Execute(\"onActionEnd.String.{0}\");", strActionName);
-            sw.Close();
+            string strActionName = oEventParameterString.String;
+
+            // Registrar el evento en el archivo
+            LogEvent(strActionName);           
         }
         catch (InvalidCastException exc)
         {
             System.Windows.Forms.MessageBox.Show("Parameter error: " + exc.Message, "MyEventHandler");
         }
+
         return 0;
     }
+
+    private void LogEvent(string actionName)
+    {
+        try
+        {
+            using (StreamWriter sw = File.AppendText(LOG_FILE_PATH))
+            {
+                sw.WriteLine("oCLI.Execute(\"onActionEnd.String.{0}\");", actionName);
+            }
+        }
+        catch (InvalidCastException exc)
+        {
+            System.Windows.Forms.MessageBox.Show("Parameter error: " + exc.Message, "MyEventHandler");
+        }
+    }
+
+    
 }
